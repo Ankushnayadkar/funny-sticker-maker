6e2138fdb62ab5bbdee88440c82e05247a3190b4
Ankushnayadkar <113438995+Ankushnayadkar@users.noreply.github.com>
2026-02-16 17:39:50 +0530

    Update library.js

--- library.js
+++ library.js
@@ -1,34 +1,33 @@
 const LIBRARY = {
 funny: [
-"Life is short. Smile while you still have teeth 😂",
-"I’m not lazy, I’m energy efficient",
-"I need 6 months vacation twice a year",
-"My brain has too many tabs open",
-"404 Motivation Not Found"
+"Life is short 😂",
+"I’m not lazy",
+"404 Motivation",
+"My brain has tabs open",
+"Too tired to function"
 ],
 
 morning: [
-"Good morning sunshine ☀️",
-"Rise & grind 💪",
-"New day new chaos 😌",
-"Wake up and be awesome",
-"Coffee first. World later ☕"
+"Good morning ☀️",
+"Rise & grind",
+"Coffee first ☕",
+"New day new chaos",
+"Wake up legend"
 ],
 
 office: [
-"Hope this email finds you well.",
-"Please find attached.",
-"Let’s circle back on this.",
-"As discussed earlier…",
-"Kind reminder 🙂"
+"Hope this email finds you well",
+"Please find attached",
+"As discussed earlier",
+"Kind reminder",
+"Let’s circle back"
 ],
 
 savage: [
-"I’m not arguing. I’m explaining why I’m right.",
-"Silence is the best reply to fools.",
-"Too glam to give a damn.",
-"I’m not mean. I’m honest.",
-"Stay mad 😎"
+"I’m always right 😎",
+"Stay mad",
+"Too glam to care",
+"Silence is power",
+"I said what I said"
 ]
 };
-
