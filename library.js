013667efcac37c1d0e12824967fd85bbce606f90
Ankushnayadkar <113438995+Ankushnayadkar@users.noreply.github.com>
2026-02-16 17:37:08 +0530

    Update library.js

--- library.js
+++ library.js
@@ -1,1 +1,34 @@
+const LIBRARY = {
+funny: [
+"Life is short. Smile while you still have teeth 😂",
+"I’m not lazy, I’m energy efficient",
+"I need 6 months vacation twice a year",
+"My brain has too many tabs open",
+"404 Motivation Not Found"
+],
+
+morning: [
+"Good morning sunshine ☀️",
+"Rise & grind 💪",
+"New day new chaos 😌",
+"Wake up and be awesome",
+"Coffee first. World later ☕"
+],
+
+office: [
+"Hope this email finds you well.",
+"Please find attached.",
+"Let’s circle back on this.",
+"As discussed earlier…",
+"Kind reminder 🙂"
+],
+
+savage: [
+"I’m not arguing. I’m explaining why I’m right.",
+"Silence is the best reply to fools.",
+"Too glam to give a damn.",
+"I’m not mean. I’m honest.",
+"Stay mad 😎"
+]
+};
 
